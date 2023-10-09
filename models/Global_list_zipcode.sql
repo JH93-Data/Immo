47117f67631b3b8db735a8e1c24d9923b8a07696
henonjeremy@gmail.com <henonjeremy@gmail.com>
2023-10-09 09:27:33 +0000

    liste des communes finalisées

---
+++ models/Global_list_zipcode.sql
@@ -0,0 +1,13 @@
+{{ config(materialized='table')}}
+
+
+SELECT Code
+    , CASE
+        WHEN Province =  "Bruxelle" then "Région de Bruxelle"
+        WHEN Province = "Brabant wallon" or  Province = "Liège" or Province = "Namur" or Province = "Hainaut" or Province = "Luxembourg" then "Région Wallonne"
+        WHEN Province = "Brabant flamand" or Province = "Antwerpen" or Province ="Brabant flamand" or Province = "Limbourg" or Province = "Flandre-Occidentale" or Province = "Flandre-Orientale" then "Région Flamande"
+       END as Region 
+    ,Province
+    ,Commune
+    ,Sub_Commune
+FROM {{ ref('List_commune') }}
