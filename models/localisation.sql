e8423249ad3fa461c32907c11465bdb26b7d00e5
henonjeremy@gmail.com <henonjeremy@gmail.com>
2023-11-20 21:44:20 +0000

    to delete

--- models/localisation.sql
+++ models/localisation.sql
@@ -15,7 +15,7 @@ SELECT ID
   ,box
   ,CASE
     WHEN number is Not NULL and box is null then CONCAT(street,", ", number)
-    WHEN number is Not NULL and box is Not null then CONCAT(street,", ", number, '/', box)
+    WHEN number is Not NULL and box is Not null then CONCAT(street,", ", number, "/"", box)
     ELSE street
   END as address
   ,CAST(latitude as string) as latitude
