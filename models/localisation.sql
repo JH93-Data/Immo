135f1898a40108c39879385663976f275ce9680b
henonjeremy@gmail.com <henonjeremy@gmail.com>
2023-11-20 21:55:43 +0000

    correction localisation faite

--- models/localisation.sql
+++ models/localisation.sql
@@ -1,13 +1,6 @@
 
 {{ config(materialized='table')}}
 
-WITH db_loca as (
-
-SELECT *
-    ,RANK() over(PARTITION BY ID order by ID, Time) as rank_2
-FROM {{ ref('Main_Unique') }}
-)
-
 SELECT ID
   ,postalCode	
   ,street	
@@ -15,10 +8,9 @@ SELECT ID
   ,box
   ,CASE
     WHEN number is Not NULL and box is null then CONCAT(street,", ", number)
-    WHEN number is Not NULL and box is Not null then CONCAT(street,", ", number, '/', box)
+    WHEN number is Not NULL and box is Not null then CONCAT(street,", ", number, "/", box)
     ELSE street
-  END as address
+   END as address
   ,CAST(latitude as string) as latitude
   ,CAST(longitude as string) as longitude
-FROM db_loca
-WHERE rank_2 = 1
+FROM {{ ref('Main_Unique') }}
